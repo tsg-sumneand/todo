f9767e9e7faeb61f8b369ff00570eb7df297e61a
Andrew Sumner <andrew_sumner@flutterint.com>
2022-05-17 14:26:55 +0100

    initial remote commit

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+import * as inquirer from 'inquirer';
+
+let todos: TodoItem[] = [
+    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)
+];
+
+let collection: TodoCollection = new TodoCollection("Andrew", todos);
+
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${ collection.getItemCounts().incomplete } items to do)`);
+    collection.getTodoItems(true).forEach(item => item.printDetails());
+}
+
+enum Commands {
+    Quit = "Quit"
+}
+
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    })
+}
+
+promptUser();
+
+//console.clear();
+//console.log(`${collection.userName}'s Todo List ` 
+//    + `(${ collection.getItemCounts().incomplete } items to do)`);
+//console.log(`${collection.userName}'s Todo List`);
+//let newId: number = collection.addTodo("Go for run");
+//let todoItem: TodoItem = collection.getTodoById(newId);
+//console.log(JSON.stringify(todoItem));
+//todoItem.printDetails();
+//collection.addTodo(todoItem);
+//collection.removeComplete();
+//collection.getTodoItems(true).forEach(item => item.printDetails());
